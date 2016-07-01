03bd4f8a55282644ffb5be0a4e9422101801df67
javier-robles <fjavier.robles@gmail.com>
2016-07-01 11:21:42 +0200

    Ejemplo Code School

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'racing-app',
+  template: `
+  <h1>{{heading}}</h1>
+  <h2>{{race.name}}</h2>
+  <p>{{race.date}}</p>
+  <p>{{race.about}}</p>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  heading = "Ultra Racing Schedule"
+  race = {
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Race through the ruins of an ancient Florida battle arena.",
+    "entryFee": 3200
+  }
+}
